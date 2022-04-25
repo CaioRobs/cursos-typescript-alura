8ef1445d76e573dc39357fd4d7d879dd0c8c92f8
Caio Vieira <caio.robs98@gmail.com>
2022-04-25 17:00:20 -0300

    prepare for third course

--- app/src/controllers/NegociacaoController.ts
+++ app/src/controllers/NegociacaoController.ts
@@ -1,24 +1,24 @@
-import { logExecutionTime } from "../decorators/executionTimeLogger.js";
-import { Weekdays } from "../enums/Weekdays.js";
-import { Negociacao } from "../models/negociacao.js";
-import { Negociacoes } from "../models/negociacoes.js";
-import { MensagemView } from "../views/MensagemView.js";
-import { NegociacoesView } from "../views/NegociacoesView.js";
+import {logExecutionTime} from '../decorators/executionTimeLogger.js';
+import {Weekdays} from '../enums/Weekdays.js';
+import {Negociacao} from '../models/negociacao.js';
+import {Negociacoes} from '../models/negociacoes.js';
+import {MensagemView} from '../views/MensagemView.js';
+import {NegociacoesView} from '../views/NegociacoesView.js';
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
   private negociacoes: Negociacoes = new Negociacoes();
-  private negociacoesView = new NegociacoesView("#negociacoes-view", true);
-  private mensagemView = new MensagemView("#mensagem-view");
+  private negociacoesView = new NegociacoesView('#negociacoes-view', true);
+  private mensagemView = new MensagemView('#mensagem-view');
 
   constructor() {
-    this.inputData = document.querySelector("#data") as HTMLInputElement;
+    this.inputData = document.querySelector('#data') as HTMLInputElement;
     this.inputQuantidade = document.querySelector(
-      "#quantidade"
+        '#quantidade',
     ) as HTMLInputElement;
-    this.inputValor = document.querySelector("#valor") as HTMLInputElement;
+    this.inputValor = document.querySelector('#valor') as HTMLInputElement;
     this.negociacoesView.update(this.negociacoes);
   }
 
@@ -26,12 +26,12 @@ export class NegociacaoController {
   public adiciona(): void {
     // aqui tem um comentario. na dist nao, por conta da config removeComments
     const negociacao = Negociacao.criaDeStrings(
-      this.inputData.value,
-      this.inputQuantidade.value,
-      this.inputValor.value
+        this.inputData.value,
+        this.inputQuantidade.value,
+        this.inputValor.value,
     );
     if (!this.ehDiaUtil(negociacao.data)) {
-      this.mensagemView.update("Apenas dias úteis!");
+      this.mensagemView.update('Apenas dias úteis!');
       return;
     }
     this.negociacoes.adiciona(negociacao);
@@ -45,14 +45,14 @@ export class NegociacaoController {
   }
 
   private limparFormulario(): void {
-    this.inputData.value = "";
-    this.inputQuantidade.value = "";
-    this.inputValor.value = "";
+    this.inputData.value = '';
+    this.inputQuantidade.value = '';
+    this.inputValor.value = '';
     this.inputData.focus();
   }
 
   private atualizaView(): void {
     this.negociacoesView.update(this.negociacoes);
-    this.mensagemView.update("Negociacao adicionada com sucesso!");
+    this.mensagemView.update('Negociacao adicionada com sucesso!');
   }
 }
